13c2111cc9223eb1c7bcbb127e8d144cb7ad800a
sonia <soniahorne12@gmail.com>
2023-01-25 10:25:24 +0100

    Initial Commit

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ def register():
     return render_template("register.html")
 
 
-@app.route('/login', methods=["GEt", "POST"])
+@app.route('/login', methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         email = request.form.get('email')
